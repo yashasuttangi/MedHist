b890026a6c3ad52c32602047ab2484ce507fdaa0
aditya-adiga <54986864+aditya-adiga@users.noreply.github.com>
2020-12-20 23:24:19 +0530

    The ui interface for the dashboard has been set up.
    buttons have been updated image added for background.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,105 +12,141 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         backgroundColor: Colors.blue[100],
-        body: Center(
+        body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/doctor_image.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
             child: Column(children: <Widget>[
-          SizedBox(height: 20),
-          SizedBox(
-            height: 80,
-            width: 400,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: Text(
-                "Hi Doctor,",
-                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.start,
+              SizedBox(height: 20),
+              SizedBox(
+                height: 80,
+                width: 400,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  child: Text(
+                    "Hi Doctor,",
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[400]),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
               ),
-            ),
-          ),
-          // Text(
-          //   "Hi Doctor",
-          //   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-          //   textAlign: TextAlign.start,
-          // ),
-          // Text(
-          //   "Welcome to your dashboard",
-          //   style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-          //   textAlign: TextAlign.left,
-          // ),
-          SizedBox(
-            height: 150,
-            width: 400,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: Text(
-                "Welcome to your dashboard",
-                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.start,
+              // Text(
+              //   "Hi Doctor",
+              //   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              //   textAlign: TextAlign.start,
+              // ),
+              // Text(
+              //   "Welcome to your dashboard",
+              //   style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              //   textAlign: TextAlign.left,
+              // ),
+
+              SizedBox(
+                height: 150,
+                width: 400,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  child: Text(
+                    "Welcome to your dashboard",
+                    style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[400]),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
               ),
-            ),
-          ),
-          SizedBox(height: 40),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            Padding(
-                padding: EdgeInsets.all(20.0),
-                child: SizedBox(
-                    height: 100.0,
-                    width: 150.0,
-                    child: Container(
-                      ///////// NEED TO WORK ON SHADOW FOR THE BUTTON and /// CONVERT ALL THE BUTTONS TO RAISED BUTTON.
-                      decoration: BoxDecoration(
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                            color: Colors.blue.withOpacity(0.1),
-                            blurRadius: 10,
-                            offset: Offset(0, 2),
-                          ),
-                        ],
-                      ),
-                      child: RaisedButton(
-                        onPressed: () => {},
-                        color: Colors.white,
-                        child: Text("Add Record"),
-                        splashColor: Colors.blue[100],
-                      ),
-                    ))),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: MaterialButton(
-                onPressed: () => {},
-                height: 100.0,
-                minWidth: 150.0,
-                color: Colors.white,
-                child: Text("Recent Patient Log"),
-                splashColor: Colors.blue[100],
-              ),
-            ),
-          ]),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: MaterialButton(
-                onPressed: () => {},
-                height: 100.0,
-                minWidth: 150.0,
-                color: Colors.white,
-                child: Text("Search Patient"),
-                splashColor: Colors.blue[100],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: MaterialButton(
-                onPressed: () => {},
-                height: 100.0,
-                minWidth: 150.0,
-                color: Colors.white,
-                child: Text("Chatbox"),
-                splashColor: Colors.blue[100],
-              ),
-            ),
-          ])
-        ])));
+              SizedBox(height: 40),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: SizedBox(
+                            height: 100.0,
+                            width: 150.0,
+                            child: Container(
+                              ///////// NEED TO WORK ON SHADOW FOR THE BUTTON and /// CONVERT ALL THE BUTTONS TO RAISED BUTTON.
+                              decoration: BoxDecoration(
+                                boxShadow: <BoxShadow>[
+                                  BoxShadow(
+                                    color: Colors.blue.withOpacity(0.1),
+                                    blurRadius: 10,
+                                    offset: Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: Image.asset('images/add_record.jpg'),
+                            ))),
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: SizedBox(
+                          height: 100.0,
+                          width: 150.0,
+                          child: Container(
+                            ///////// NEED TO WORK ON SHADOW FOR THE BUTTON and /// CONVERT ALL THE BUTTONS TO RAISED BUTTON.
+                            decoration: BoxDecoration(
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                  color: Colors.blue.withOpacity(0.1),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Image.asset('images/recent_record.jpg'),
+                          )),
+                    ),
+                  ]),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: SizedBox(
+                          height: 100.0,
+                          width: 150.0,
+                          child: Container(
+                            ///////// NEED TO WORK ON SHADOW FOR THE BUTTON and /// CONVERT ALL THE BUTTONS TO RAISED BUTTON.
+                            decoration: BoxDecoration(
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                  color: Colors.blue.withOpacity(0.1),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Image.asset('images/search_patients.jpg'),
+                          )),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: SizedBox(
+                          height: 100.0,
+                          width: 150.0,
+                          child: Container(
+                            ///////// NEED TO WORK ON SHADOW FOR THE BUTTON and /// CONVERT ALL THE BUTTONS TO RAISED BUTTON.
+                            decoration: BoxDecoration(
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                  color: Colors.blue.withOpacity(0.1),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Image.asset('images/doc_chat.jpg'),
+                          )),
+                    ),
+                  ])
+            ])));
   }
 }
 
